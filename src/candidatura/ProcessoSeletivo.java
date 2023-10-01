9b101328c942c14eb79dddc5957310e9b953a73c
larissaslff <110000521+larissaslff@users.noreply.github.com>
2023-10-01 15:24:50 -0300

    add entrar em contado

--- src/candidatura/ProcessoSeletivo.java
+++ src/candidatura/ProcessoSeletivo.java
@@ -1,11 +1,44 @@
 package candidatura;
 
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.Random;
 
 public class ProcessoSeletivo {
     public static void main(String[] args) throws Exception {
-        imprimirSelecionados();
+        String[] candidatos = { "Ana", "Bruno", "Cátia", "Damião", "Erica" };
 
+        for (String candidato : candidatos) {
+            entrarEmContato(candidato);
+        }
+
+    }
+
+    static void entrarEmContato(String candidato) {
+        int tentativasRealizadas = 1;
+        boolean continuarTentando = true;
+        boolean atendeu = false;
+
+        do {
+            atendeu = atender();
+            continuarTentando = !atendeu;
+            if (continuarTentando) {
+                tentativasRealizadas++;
+            } else {
+                System.out.println("Contato realizado com sucesso!");
+            }
+        } while (continuarTentando && tentativasRealizadas < 3);
+
+        if (atendeu) {
+            System.out.println("Conseguimos contato com " + candidato + " na " + tentativasRealizadas + " tentativa");
+        } else {
+            System.out.println("Não conseguimos contato com " + candidato + ", numero máximo de tentativas "
+                    + tentativasRealizadas);
+        }
+
+    }
+
+    static boolean atender() {
+        return new Random().nextInt(3) == 1;
     }
 
     static void selecaoCandidatos() {
